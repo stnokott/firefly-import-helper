6e83d245f643f83cec91921462c84c77f9214e97
stnokott <30934296+stnokott@users.noreply.github.com>
2026-07-21 13:10:10 +0200

    fix: query future times to account for tz drift

--- internal/importer/importer.go
+++ internal/importer/importer.go
@@ -138,7 +138,7 @@ func (im *Importer) Import(ctx context.Context) (err error) {
 func (im *Importer) nextImportRange() (from, to time.Time) {
 	minFrom := im.bank.MinImportTransactionTime()
 	from = slices.MaxFunc([]time.Time{im.lastRun, minFrom}, time.Time.Compare)
-	to = time.Now()
+	to = time.Now().Add(24 * time.Hour)
 	return
 }
 
